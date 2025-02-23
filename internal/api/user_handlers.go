2930b3a11fde69198db3def7730bfdae548df8f7
Thalles Oliveira <66585100+MH0R@users.noreply.github.com>
2025-02-23 14:19:20 -0300

    feat: handlers setup

---
+++ internal/api/user_handlers.go
@@ -0,0 +1,14 @@
+package api
+
+import "net/http"
+
+func (api *Api) handlerSignupUser(w http.ResponseWriter, r *http.Request) {
+	panic("TO DO NOT IMPLEMENTED")
+}
+
+func (api *Api) handlerLoginUser(w http.ResponseWriter, r *http.Request) {
+	panic("TO DO NOT IMPLEMENTED")
+}
+func (api *Api) handlerLogoutUser(w http.ResponseWriter, r *http.Request) {
+	panic("TO DO NOT IMPLEMENTED")
+}
